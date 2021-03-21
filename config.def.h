dd0e0718b02daddf31343ed1217c6eb7af098f94
wuguipeng <46239726+WUGUIPENG@users.noreply.github.com>
2021-03-21 18:00:04 +0800

    初始化

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Symbols Nerd Font:size=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -56,13 +56,23 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun" };
 static const char *termcmd[]  = { "st", NULL };
+static const char *volup[] = { "amixer", "-qM", "set", "Master", "2%+", "umute", NULL };
+static const char *voldown[] = { "amixer", "-qM", "set", "Master", "2%-", "umute", NULL }; 
+static const char *mute[] = { "amixer", "-qM", "set", "Master", "toggle", NULL };
+static const char *lightup[] = { "xbacklight", "-inc", "2", NULL };
+static const char *lightdown[] = { "xbacklight", "-dec", "2", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Up,     spawn,          {.v = lightup} },
+        { MODKEY|ShiftMask,             XK_Down,   spawn,          {.v = lightdown} },
+        { MODKEY|ShiftMask,             XK_Right,  spawn,          {.v = volup} },
+        { MODKEY|ShiftMask,             XK_Left,   spawn,          {.v = voldown} },
+        { MODKEY|ShiftMask,             XK_m,      spawn,          {.v = mute} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
